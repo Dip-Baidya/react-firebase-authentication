6a32687b85e175c532398c7105f7bf09a3c6ba39
Dip-Baidya <dipbaidya.cse@gmail.com>
2022-01-29 23:42:54 +0600

    add firebase setting file

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBsbDnTFmHRlEkre11aYQD9e5pdpvrflbE",
+    authDomain: "react-firebase-authentic-4cf4a.firebaseapp.com",
+    projectId: "react-firebase-authentic-4cf4a",
+    storageBucket: "react-firebase-authentic-4cf4a.appspot.com",
+    messagingSenderId: "140911947846",
+    appId: "1:140911947846:web:e392d5915d2dc7c2f69a47"
+};
+
+export default firebaseConfig
