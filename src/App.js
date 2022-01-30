21af4c8d908917383a59b4ebd67f8d45763f8319
Dip-Baidya <dipbaidya.cse@gmail.com>
2022-01-30 21:06:10 +0600

    Handle Email Verification, Forget Password Or Reset Password

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { getAuth, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword, signInWithEmailAndPassword, sendEmailVerification, sendPasswordResetEmail } from "firebase/auth";
 import initializeAuthentication from './firebase/firebase.initialize';
 import { useState } from 'react';
 
@@ -12,6 +12,7 @@ function App() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
+  const [isLogin, setIsLogin] = useState(false);
 
   const handleGoogleSignIn = () => {
     signInWithPopup(auth, googleProvider)
@@ -19,6 +20,10 @@ function App() {
         const user = result.user;
         console.log(user)
       })
+  }
+
+  const toggleLogIn = (event) => {
+    setIsLogin(event.target.checked)
   }
 
   const handleRegistration = (event) => {
@@ -33,11 +38,56 @@ function App() {
       setError('Password must be contain 2 Upper Case');
       return;
     }
+
+    // isLogin ? processLogin(email, password) : createNewUser(email, password)
+    if (isLogin) {
+      processLogin(email, password);
+    }
+    else {
+      registerNewUser(email, password)
+    }
+  }
+
+  const processLogin = (email, password) => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        setError('');
+      })
+      .catch((error) => {
+        setError(error.message)
+      })
+
+  }
+
+  const registerNewUser = (email, password) => {
     createUserWithEmailAndPassword(auth, email, password)
       .then((result) => {
         const user = result.user
         console.log(user)
+        setError('');
+        verifyEmail();
       })
+      .catch((error) => {
+        setError(error.message);
+      })
+  }
+
+  const verifyEmail = () => {
+    sendEmailVerification(auth.currentUser)
+      .then((result) => {
+        console.log(result)
+      })
+  }
+  const handleResetPassword = () => {
+    sendPasswordResetEmail(auth, email)
+      .then((result) => {
+        // Password reset email sent!
+        // ..
+        console.log(result);
+      })
+
   }
 
   const handleEmailChange = (event) => {
@@ -49,7 +99,7 @@ function App() {
   return (
     <div className="mx-5">
       <form onSubmit={handleRegistration}>
-        <h3 className='App text-primary'>Please Register</h3>
+        <h3 className='App text-primary'>Please {isLogin ? 'Log In' : 'Register'}</h3>
         <div className="row mb-3">
           <label htmlFor="inputEmail3" className="col-sm-2 col-form-label">Email</label>
           <div className="col-sm-10">
@@ -62,9 +112,22 @@ function App() {
             <input onBlur={handlePasswordChange} type="password" className="form-control" id="inputPassword3" required />
           </div>
         </div>
-        <div className="row mb-3 text-danger">{error}</div>
 
-        <button type="submit" className="btn btn-primary">Sign in</button>
+        <div className="row mb-3">
+          <div className="col-sm-10 offset-sm-2">
+            <div className="form-check">
+              <input onChange={toggleLogIn} className="form-check-input" type="checkbox" id="gridCheck1" />
+              <label className="form-check-label" for="gridCheck1">
+                Already Registered
+              </label>
+            </div>
+          </div>
+        </div>
+
+        <div className="row mb-3 text-danger">{error}</div>
+        <button type="submit" className="btn btn-primary">{isLogin ? 'Login' : 'Register'}</button>
+        <button onClick={handleResetPassword} type="button" className="btn btn-secondary btn-sm">Reset password</button>
+
       </form>
 
 
