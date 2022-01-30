f46faf2ccf6bc59faf076d5ebd99f07d393e931f
Dip-Baidya <dipbaidya.cse@gmail.com>
2022-01-30 16:18:01 +0600

    Create New User Using Email And Password

--- src/App.js
+++ src/App.js
@@ -1,10 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import { getAuth, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword } from "firebase/auth";
+import initializeAuthentication from './firebase/firebase.initialize';
+import { useState } from 'react';
+
+initializeAuthentication();
+const googleProvider = new GoogleAuthProvider();
 
 function App() {
-  return (
-    <div className="App">
+  const auth = getAuth();
 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, googleProvider)
+      .then(result => {
+        const user = result.user;
+        console.log(user)
+      })
+  }
+
+  const handleRegistration = (event) => {
+    console.log(password, email)
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        const user = result.user
+        console.log(user)
+      })
+    event.preventDefault()
+  }
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value)
+  }
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value)
+  }
+  return (
+    <div className="mx-5">
+      <form onSubmit={handleRegistration}>
+        <h3 className='App text-primary'>Please Register</h3>
+        <div className="row mb-3">
+          <label htmlFor="inputEmail3" className="col-sm-2 col-form-label">Email</label>
+          <div className="col-sm-10">
+            <input onBlur={handleEmailChange} type="email" className="form-control" id="inputEmail3" required />
+          </div>
+        </div>
+        <div className="row mb-3">
+          <label htmlFor="inputPassword3" className="col-sm-2 col-form-label">Password</label>
+          <div className="col-sm-10">
+            <input onBlur={handlePasswordChange} type="password" className="form-control" id="inputPassword3" required />
+          </div>
+        </div>
+
+        <button type="submit" className="btn btn-primary">Sign in</button>
+      </form>
+
+
+      <br /><br /><br /><br />
+      <div>----------------------------------------------</div>
+      <br /><br /><br /><br />
+      <button onClick={handleGoogleSignIn}>Sign In with Google</button>
     </div>
   );
 }
